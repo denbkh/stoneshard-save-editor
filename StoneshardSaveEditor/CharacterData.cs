8bc3846a49cb5b475f1b4f3232018bb61b50c3d7
Denis Bakharev <den.bkh@gmail.com>
2022-08-29 13:38:01 +0300

    Use 2 space indentations

--- StoneshardSaveEditor/CharacterData.cs
+++ StoneshardSaveEditor/CharacterData.cs
@@ -2,19 +2,19 @@ using System.ComponentModel;
 
 namespace StoneshardSaveEditor
 {
-    public class CharacterData
-    {
-        public string Name { get; set; }
-        public int Strength { get; set; }
-        public int Agility { get; set; }
-        public int Perception { get; set; }
-        public int Vitality { get; set; }
-        public int Willpower { get; set; }
-        public int AbilityPoints { get; set; }
-        public int StatsPoints { get; set; }
-        public int Level { get; set; }
-        public int XP { get; set; }
-        public string GameTime { get; set; }
-        public BindingList<string> Abilities { get; set; }
-    }
+  public class CharacterData
+  {
+    public string Name { get; set; }
+    public int Strength { get; set; }
+    public int Agility { get; set; }
+    public int Perception { get; set; }
+    public int Vitality { get; set; }
+    public int Willpower { get; set; }
+    public int AbilityPoints { get; set; }
+    public int StatsPoints { get; set; }
+    public int Level { get; set; }
+    public int XP { get; set; }
+    public string GameTime { get; set; }
+    public BindingList<string> Abilities { get; set; }
+  }
 }
--- StoneshardSaveEditor/Utils.cs
+++ StoneshardSaveEditor/Utils.cs
@@ -6,24 +6,24 @@ using Newtonsoft.Json.Linq;
 
 namespace StoneshardSaveEditor
 {
-    public static class Utils
+  public static class Utils
+  {
+    public static JObject ReadJson(string file)
     {
-        public static JObject ReadJson(string file)
+      using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+      {
+        using (var inputStream = new InflaterInputStream(fs))
         {
-            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+          using (StreamReader sr = new StreamReader(inputStream, Encoding.UTF8))
+          {
+            using (JsonTextReader reader = new JsonTextReader(sr))
             {
-                using (var inputStream = new InflaterInputStream(fs))
-                {
-                    using (StreamReader sr = new StreamReader(inputStream, Encoding.UTF8))
-                    {
-                        using (JsonTextReader reader = new JsonTextReader(sr))
-                        {
-                            reader.FloatParseHandling = FloatParseHandling.Decimal;
-                            return JObject.Load(reader);
-                        }
-                    }
-                }
+              reader.FloatParseHandling = FloatParseHandling.Decimal;
+              return JObject.Load(reader);
             }
+          }
         }
+      }
     }
+  }
 }
